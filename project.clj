4c7fe90afdcd5d7482bea34252399ec8fc6efd4b
Remco van 't Veer <remco@remworks.net>
2021-08-04 11:22:43 +0200

    Include license information

--- project.clj
+++ project.clj
@@ -1,5 +1,23 @@
+;; Copyright (C) 2021 SURFnet B.V.
+;;
+;; This program is free software: you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by the Free
+;; Software Foundation, either version 3 of the License, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful, but WITHOUT
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+;; FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+;; more details.
+;;
+;; You should have received a copy of the GNU General Public License along
+;; with this program. If not, see http://www.gnu.org/licenses/.
+
 (defproject ooapi-gateway-configurator "0.1.0-SNAPSHOT"
   :description "Configure the Surf OOAPI Gateway"
+  :license {:name "GPLv3"
+            :url  "https://www.gnu.org/licenses/gpl-3.0.en.html"}
+  :url "https://github.com/SURFnet/ooapi-gateway-configurator"
 
   :dependencies [[cheshire "5.10.0"]
                  [ch.qos.logback/logback-classic "1.2.3"]
