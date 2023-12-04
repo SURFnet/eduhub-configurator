b99f99d87a341a111dee6f79e7056dc29fd831d7
Remco van 't Veer <remco@remworks.net>
2023-12-04 10:16:58 +0100

    Update logback dependency
    Update for logback for CVE-2023-6378.

--- project.clj
+++ project.clj
@@ -21,7 +21,7 @@
 
   :dependencies [[ch.qos.logback.contrib/logback-jackson "0.1.5"]
                  [ch.qos.logback.contrib/logback-json-classic "0.1.5"]
-                 [ch.qos.logback/logback-classic "1.4.11"]
+                 [ch.qos.logback/logback-classic "1.4.14"]
                  [clj-commons/clj-yaml "1.0.27"]
                  [compojure "1.7.0"]
                  [datascript "1.5.4"]
