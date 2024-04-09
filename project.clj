53ed2447b85375b5e6542d8e9eafbf7b0a1fc427
Remco van 't Veer <remco@remworks.net>
2024-04-09 16:42:07 +0200

    Update deps

--- project.clj
+++ project.clj
@@ -19,7 +19,7 @@
             :url  "https://www.gnu.org/licenses/gpl-3.0.en.html"}
   :url "https://github.com/SURFnet/ooapi-gateway-configurator"
 
-  :dependencies [[org.clojure/clojure "1.11.1"]
+  :dependencies [[org.clojure/clojure "1.11.2"]
 
                  ;; setup
                  [environ "1.2.0"]
@@ -29,25 +29,25 @@
                  [compojure "1.7.1"]
                  [hiccup "1.0.5"]
                  [ring-oauth2 "0.2.2"] ;; TODO waarom kan die?
-                 [ring/ring-core "1.11.0"]
+                 [ring/ring-core "1.12.1"]
                  [ring/ring-defaults "0.4.0"]
-                 [ring/ring-jetty-adapter "1.11.0"]
+                 [ring/ring-jetty-adapter "1.12.1"]
                  [nl.jomco/clj-http-status-codes "0.1"]
                  [nl.jomco/ring-trace-context "0.0.8"]
 
                  ;; data
                  [org.clojure/data.json "2.5.0"]
-                 [org.clojure/core.cache "1.0.225"]
+                 [org.clojure/core.cache "1.1.234"]
                  [clj-commons/clj-yaml "1.0.27"]
-                 [datascript "1.6.1"]
+                 [datascript "1.6.3"]
 
                  ;; logging
-                 [org.clojure/tools.logging "1.2.4"]
+                 [org.clojure/tools.logging "1.3.0"]
                  [ch.qos.logback.contrib/logback-jackson "0.1.5"]
                  [ch.qos.logback.contrib/logback-json-classic "0.1.5"]
-                 [ch.qos.logback/logback-classic "1.4.14"]
-                 [com.fasterxml.jackson.core/jackson-core "2.16.1"]
-                 [com.fasterxml.jackson.core/jackson-databind "2.16.1"]]
+                 [ch.qos.logback/logback-classic "1.5.4"]
+                 [com.fasterxml.jackson.core/jackson-core "2.17.0"]
+                 [com.fasterxml.jackson.core/jackson-databind "2.17.0"]]
 
   :main ^:skip-aot ooapi-gateway-configurator.core
 
